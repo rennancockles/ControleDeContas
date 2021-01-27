b293d053964ae86452b9f021259c17221f9560b8
Rennan Cockles <rennancockles@gmail.com>
2021-01-27 16:03:38 -0300

    login route

---
+++ server/src/main/routes/auth-routes.ts
@@ -0,0 +1,10 @@
+import { adaptRoute } from '@/main/adapters'
+// import { makeSignUpController, makeLoginController } from '@/main/factories'
+import { makeLoginController } from '@/main/factories'
+
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  // router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
+}
