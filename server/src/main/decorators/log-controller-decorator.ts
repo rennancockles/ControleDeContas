b5cda58954dba3654d86fab4b95ce75857ab5ed1
Rennan Cockles <rennancockles@gmail.com>
2021-01-28 16:13:05 -0300

    update logController decorator to receive errorLogger as param

--- server/src/main/decorators/log-controller-decorator.ts
+++ server/src/main/decorators/log-controller-decorator.ts
@@ -1,13 +1,17 @@
+import { IErrorLogger } from '@/infra/log'
 import { IController, IHttpResponse } from '@/presentation/protocols'
 
 export class LogControllerDecorator implements IController {
-  constructor (private readonly controller: IController) {}
+  constructor (
+    private readonly controller: IController,
+    private readonly errorLogger: IErrorLogger
+  ) {}
 
   async handle (request: any): Promise<IHttpResponse> {
     const httpResponse = await this.controller.handle(request)
 
     if (httpResponse.statusCode === 500) {
-      // log error
+      this.errorLogger.logError(httpResponse.body.stack)
     }
 
     return httpResponse
