0be663b3e4a21346daa139be073891eb16ffc573
Rennan Cockles <rennancockles@gmail.com>
2021-01-28 16:10:12 -0300

    errorLogger interface, composite and test

--- server/tests/validation/validation-composite.spec.ts
+++ server/tests/validation/validation-composite.spec.ts
@@ -43,7 +43,7 @@ describe('Validation Composite', () => {
     expect(error).toEqual(validationSpies[0].error)
   })
 
-  test('Should not return if validation succeeds', () => {
+  it('Should not return if validation succeeds', () => {
     const { sut } = makeSut()
 
     const error = sut.validate({ [field]: faker.random.word() })
