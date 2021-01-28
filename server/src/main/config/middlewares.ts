83a7916f4ffe447a9388c0c8ae675b535a554540
Rennan Cockles <rennancockles@gmail.com>
2021-01-28 17:02:21 -0300

    bodyParser middleware

---
+++ server/src/main/config/middlewares.ts
@@ -0,0 +1,10 @@
+// import { bodyParser, cors, contentType } from '@/main/middlewares'
+import { bodyParser } from '@/main/middlewares'
+
+import { Express } from 'express'
+
+export default (app: Express): void => {
+  app.use(bodyParser)
+  // app.use(cors)
+  // app.use(contentType)
+}
