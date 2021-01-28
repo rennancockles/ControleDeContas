8fa79bddcb8af5db9b58686ee0ec228b5ce0453d
Rennan Cockles <rennancockles@gmail.com>
2021-01-28 16:30:08 -0300

    add erroLogger to logControllerDecorator factory

--- server/src/main/factories/decorators/log-controller-decorator-factory.ts
+++ server/src/main/factories/decorators/log-controller-decorator-factory.ts
@@ -1,6 +1,9 @@
+import { ErrorLoggerComposite, IErrorLogger } from '@/infra/log'
 import { LogControllerDecorator } from '@/main/decorators'
 import { IController } from '@/presentation/protocols'
 
 export const makeLogControllerDecorator = (controller: IController): IController => {
-  return new LogControllerDecorator(controller)
+  const loggers:IErrorLogger[] = []
+
+  return new LogControllerDecorator(controller, new ErrorLoggerComposite(loggers))
 }
