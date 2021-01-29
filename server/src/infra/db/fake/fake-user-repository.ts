8544f0183abc5724d6de9fb5e5064eb93f1a510e
Rennan Cockles <rennancockles@gmail.com>
2021-01-29 15:56:47 -0300

    fix fakeUser repository to not hash password on get user methods

--- server/src/infra/db/fake/fake-user-repository.ts
+++ server/src/infra/db/fake/fake-user-repository.ts
@@ -1,6 +1,5 @@
 import { ICheckUserByEmailRepository, ICreateUserRepository, IGetUserByEmailRepository, IGetUserByIdRepository } from '@/data/protocols'
 
-import { hash } from 'bcrypt'
 import faker from 'faker'
 
 const usersMock = [
@@ -9,34 +8,24 @@ const usersMock = [
     name: 'Rennan',
     lastname: 'Cockles',
     email: 'rennan@teste.com',
-    password: '123456'
+    password: '$2b$12$qa3KYIwdjUVNhM107628.eh1BEG9r6QSrlMhloFXbMQHAaq2nWjZi'
   },
   {
     id: '2',
     name: 'João',
     lastname: 'Ninguém',
     email: 'joao@teste.com',
-    password: '123'
+    password: '$2b$12$fH/J.yzPrO2YD6/OWg0i2ewG/AoHZFIdRRv4M15VvwrkV4fy/VgSS'
   }
 ]
 
 export class FakeUserRepository implements IGetUserByEmailRepository, ICreateUserRepository, ICheckUserByEmailRepository, IGetUserByIdRepository {
   async getByEmail (email: string): Promise<IGetUserByEmailRepository.Result> {
-    let user = usersMock.find(user => user.email === email)
-    if (user) {
-      const hashedPassword = await hash(user.password, 12)
-      user = { ...user, password: hashedPassword }
-    }
-    return user
+    return usersMock.find(user => user.email === email)
   }
 
   async getById (id: string): Promise<IGetUserByIdRepository.Result> {
-    let user = usersMock.find(user => user.id === id)
-    if (user) {
-      const hashedPassword = await hash(user.password, 12)
-      user = { ...user, password: hashedPassword }
-    }
-    return user
+    return usersMock.find(user => user.id === id)
   }
 
   async create (userData: ICreateUserRepository.Params): Promise<ICreateUserRepository.Result> {
@@ -47,7 +36,6 @@ export class FakeUserRepository implements IGetUserByEmailRepository, ICreateUse
 
   async checkByEmail (email: string): Promise<ICheckUserByEmailRepository.Result> {
     const user = await this.getByEmail(email)
-
     return Boolean(user)
   }
 }
