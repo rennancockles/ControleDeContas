cd5e2502991cd976f06e19254c5dc747f0055e9c
Rennan Cockles <rennancockles@gmail.com>
2021-01-29 16:36:22 -0300

    graphql auth middleware

--- server/src/main/config/apollo-server.ts
+++ server/src/main/config/apollo-server.ts
@@ -1,6 +1,6 @@
 import typeDefs from '@/main/graphql/type-defs'
 import resolvers from '@/main/graphql/resolvers'
-// import schemaDirectives from '@/main/graphql/directives'
+import schemaDirectives from '@/main/graphql/directives'
 
 import { ApolloServer } from 'apollo-server-express'
 import { Express } from 'express'
@@ -30,7 +30,7 @@ export default (app: Express): void => {
   const server = new ApolloServer({
     resolvers,
     typeDefs,
-    // schemaDirectives,
+    schemaDirectives,
     context: ({ req }) => ({ req }),
     plugins: [{
       requestDidStart: () => ({
