f9dff3f9f18cf3e7968dab7b6c81adabf2c262c0
Rennan Cockles <rennancockles@gmail.com>
2021-01-29 17:17:54 -0300

    swagger config

--- server/src/main/config/app.ts
+++ server/src/main/config/app.ts
@@ -1,12 +1,14 @@
 import setupRoutes from '@/main/config/routes'
 import setupMiddlewares from '@/main/config/middlewares'
 import setupApolloServer from '@/main/config/apollo-server'
+import setupSwagger from '@/main/config/swagger'
 
 import express from 'express'
 
 const app = express()
 
 setupApolloServer(app)
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 
