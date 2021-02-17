d2f15f10b714f4e3bc7184c4bd8b4f57ce8570c4
Rennan Cockles <rennancockles@gmail.com>
2021-02-17 14:26:07 -0300

    mailer mock

--- server/tests/data/mocks/mock-mailer.ts
+++ server/tests/data/mocks/mock-mailer.ts
@@ -1,11 +1,15 @@
 import { IRecoverMailSender } from '@/data/protocols'
 
 export class RecoverMailSenderSpy implements IRecoverMailSender {
+  name: string
   email: string
+  recoverUrl: string
   executionAmount: number = 0
 
-  async send (email: string): Promise<void> {
+  async sendRecoverEmail (name: string, email: string, recoverUrl: string): Promise<void> {
+    this.name = name
     this.email = email
+    this.recoverUrl = recoverUrl
     this.executionAmount += 1
   }
 }
