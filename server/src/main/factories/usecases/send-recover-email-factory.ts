11c686bf2a23e10f577fbe2b6a058981a1693e25
Rennan Cockles <rennancockles@gmail.com>
2021-02-17 15:28:26 -0300

    update send recover email usecase factory

--- server/src/main/factories/usecases/send-recover-email-factory.ts
+++ server/src/main/factories/usecases/send-recover-email-factory.ts
@@ -1,11 +1,14 @@
 import { SendRecoverEmail } from '@/data/usecases'
 import { ISendRecoverEmail } from '@/domain/usecases'
 import { FakeUserRepository } from '@/infra/db'
+import { CryptoAdapter } from '@/infra/encryption'
+import env from '@/main/config/env'
 import { makeMailSender } from '../email'
 
 export const makeSendRecoverEmailUsecase = (): ISendRecoverEmail => {
+  const cryptoAdapter = new CryptoAdapter(env.cryptoAlg, env.cryptoKey, env.cryptoIV)
   const mailSender = makeMailSender()
   const fakeUserRepository = new FakeUserRepository()
 
-  return new SendRecoverEmail(mailSender, fakeUserRepository)
+  return new SendRecoverEmail(mailSender, fakeUserRepository, cryptoAdapter)
 }
